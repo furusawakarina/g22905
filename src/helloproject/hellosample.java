b71adc341eed5875112962d625757e2a04f3ae0e
furusawakarina <furusawakarina@localhost>
2024-05-10 17:45:44 +0900

    seconde

--- src/helloproject/hellosample.java
+++ src/helloproject/hellosample.java
@@ -3,7 +3,7 @@ package helloproject;
 public class hellosample {
 
 	public static void main(String[] args) {
-		System.out.println("helloworld");
+		System.out.println("helloworld!!!!!");
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
